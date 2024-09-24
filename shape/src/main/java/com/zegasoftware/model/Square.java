93c4410b760b0668c2de3682413cbbb68575a7f0
Cucu Antonia <ioana.cucu@student.unitbv.ro>
2024-09-24 13:01:02 +0300

    Update Triangle and Square classes

--- shape/src/main/java/com/zegasoftware/model/Square.java
+++ shape/src/main/java/com/zegasoftware/model/Square.java
@@ -1,4 +1,16 @@
 package com.zegasoftware.model;
 
-public class Square {
+public class Square extends Shape {
+
+    int a;
+
+    @Override
+    public double area() {
+        return a*a;
+    }
+
+    @Override
+    public double perimeter() {
+        return 4*a;
+    }
 }
--- shape/src/main/java/com/zegasoftware/model/Triangle.java
+++ shape/src/main/java/com/zegasoftware/model/Triangle.java
@@ -1,4 +1,6 @@
 package com.zegasoftware.model;
+import java.lang.Math;
+
 
 public class Triangle extends Shape {
     private int a;
@@ -7,6 +9,13 @@ public class Triangle extends Shape {
 
     @Override
     public double area() {
+        double s = (a+b+c)/2;
+        return Math.sqrt(s*(s-a)*(s-b)*(s-c));
 
+    }
+
+    @Override
+    public double perimeter() {
+        return a+b+c;
     }
 }
